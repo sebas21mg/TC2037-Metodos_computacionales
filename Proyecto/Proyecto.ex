e9f3928ca7c6edcc82aca892acabc6825d790b12
Sebastian Moncada <sebas.200221@gmail.com>
2023-05-31 14:12:17 -0600

    Ya funciona el html

--- Proyecto/Proyecto.ex
+++ Proyecto/Proyecto.ex
@@ -1,48 +1,44 @@
-        
 # Sebastian Moncada - A01027028
 # Samuel Acevedo - A01026893
 
 defmodule FileIO do
 
   @htmlBeforeTitle "<!DOCTYPE html>\n<html lang='es'>\n<head>\n<meta charset='UTF-8'>\n<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n<title>"
-  @htmlAfterTitle "</title>\n</head>\n<body>\n"
-  @htmlEnd "\n</body>\n</html>"
+  @htmlAfterTitle "</title>\n</head>\n<body>\n<pre>\n"
+  @htmlEnd "\n</pre>\n</body>\n</html>"
+
+  def regexPruebas(string) do
+    Regex.match?(~r/hola/, string) |> IO.puts()
+  end
+
+  def replace(string) do
+    Regex.replace(~r/(def|return|print|str)/, string, "<span style='color: blue;'>\\0</span>")
+    Regex.replace(~r/\w+/, string, "<span style='color: orange;'>\\0</span>")
+  end
 
   def lineSplit(line) do
     line 
-    |> String.split(" ") 
-    |> IO.inspect() # Equivalente a un debug.log
+      |> String.split(" ") 
+      |> Enum.map(&replace(&1))
+      |> Enum.join(" ")
   end
 
   def highlightSyntax(inFilePath, outFilePath) do
-    # file = inFilePath 
-    #   |> File.stream!()
-      # |> Enum.map(&line_shift(&1, offset)) # Hacer el cifrado
+    fileName = inFilePath |> Path.basename()
 
-    fileName = inFilePath |> Path.basename() |> IO.inspect()
+    fileContent = inFilePath 
+      |> File.stream!() # Tomar las líneas del archivo
+      |> Enum.map(&lineSplit(&1)) # Hacer el cifrado
+      |> Enum.join("\n") # Juntar todas las líneas
 
-    File.write(outFilePath, Enum.join([@htmlBeforeTitle, fileName, @htmlAfterTitle, "<h1>Hola mundo</h1>", @htmlEnd]))
+    File.write(outFilePath, Enum.join([@htmlBeforeTitle, fileName, @htmlAfterTitle, fileContent, @htmlEnd]))
   end
-
-  # def options(string) do
-  #   cond do
-  #   end
-  # end
-
-  def replace() do
-    # Regex.replace(~r/Weight: (\d*)g/, "Weight: 74g", "Gewicht: \\1g")
-    reserved_words = ~r/(def|return|print|str)/
-    Regex.replace(reserved_words, "def", "<span style='color: blue;'>\\1</span>") |> IO.puts()
-    IO.puts(" suma(a, b):")
-  end
-
-  # def compare(string) do
-  #   Regex.match?(reserved_words, string)
-  # end
 
 end
 
 # IO.puts(FileIO.compare("def"))
-# IO.puts(FileIO.replace())
-IO.puts(FileIO.lineSplit("def suma(a, b):"))
-# IO.puts(FileIO.highlightSyntax("./Ejemplo.py", "./Proyecto/Output.html"))
+# IO.puts(FileIO.replace("def"))
+# FileIO.lineSplit("def suma(a, b):")
+
+FileIO.highlightSyntax('./Proyecto/Ejemplo.py', './Proyecto/Output.html')
+# FileIO.regexPruebas("fruechor")
